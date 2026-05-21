fb3cbb8a1242e5c2aa84249db4a09ef4f1110043
LinoX-m1 <zekouzb2025@gmail.com>
2026-05-21 17:58:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,21 @@
-import os
-import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
-from moviepy.editor import VideoFileClip
 
-# 🔥 Faqat bitta TOKEN bo‘ladi
 TOKEN = "8928147933:AAF2A4YVxQLOnJM6fxzg46oPsIhFnmWm53g"
 ADMIN_ID = 7818670765
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-logging.basicConfig(level=logging.INFO)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.reply("Salom 👋 Men oddiy botman!")
 
 
-def extract_audio(video_path, audio_path):
-    video = VideoFileClip(video_path)
-    video.audio.write_audiofile(audio_path)
-    video.close()
-
-
-@dp.message_handler(content_types=['video'])
-async def handle_video(message: types.Message):
-    user = message.from_user
-
-    logging.info(f"User: {user.id} @{user.username}")
-
-    file = await bot.get_file(message.video.file_id)
-
-    video_path = f"video_{user.id}.mp4"
-    audio_path = f"audio_{user.id}.mp3"
-
-    await bot.download_file(file.file_path, video_path)
-
-    await message.reply("Video qabul qilindi...")
-
-    extract_audio(video_path, audio_path)
-
-    await message.reply_audio(types.InputFile(audio_path), caption=f"User ID: {user.id}")
-
-    os.remove(video_path)
-    os.remove(audio_path)
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.reply(message.text)
 
 
 if __name__ == "__main__":
